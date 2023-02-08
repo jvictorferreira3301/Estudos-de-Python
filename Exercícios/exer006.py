24b7dc7b0ceab081844147d2387b15f14ec41f91
João Victor Ferreira <jvictorsbferreira@gmail.com>
2023-02-08 10:38:30 -0300

    Create exer006.py

---
+++ Exercícios/exer006.py
@@ -0,0 +1,5 @@
+n=int(input('Insira um número: '))
+dn=n*2 #dobro
+tn=n*3 #triplo
+rn=n**(1/2) #raiz quadrada
+print('\nResultados:\nO dobro de {} é {}\nO Triplo de {} é {}\nA raiz de {} é {}'.format(n,dn,n,tn,n,rn))
