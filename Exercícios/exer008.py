b20c52b39c12c8204a67ebfa1f60bece2bd93821
João Victor Ferreira <jvictorsbferreira@gmail.com>
2023-02-08 13:00:40 -0300

    exer009

---
+++ Exercícios/exer008.py
@@ -0,0 +1,4 @@
+met=int(input('Insira(metros): '))
+cen=met*100
+mil=met*1000
+print('\n{} metro(s) equivale(m) a {} centímetro(s)\n{} metro(s) equivale(m) a {} milímetro(s)'.format(met,cen,met,mil))
